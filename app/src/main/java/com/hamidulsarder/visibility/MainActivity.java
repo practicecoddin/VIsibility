9309b63708a010e477a3fb13c94cf82a83780956
user <practicecoddin@gmail.com>
2023-06-06 07:05:20 +0600

    Final

--- app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
+++ app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     CardView bBS, bES, bHS, bTS;
 
-    TextView git;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
