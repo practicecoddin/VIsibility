dfd815f85eb8a88ab6bb679a3f0b0c4abbbabc7b
user <practicecoddin@gmail.com>
2023-06-07 07:25:57 +0600

    S.M.Hamidul

--- app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
+++ app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     CardView bBS, bES, bHS, bTS;
 
-    TextView checkHamidul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
