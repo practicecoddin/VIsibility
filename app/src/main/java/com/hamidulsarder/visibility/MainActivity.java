252d6c50a4c6c8ee432e2cf4270195f02930a739
user <practicecoddin@gmail.com>
2023-06-07 07:27:53 +0600

    S.M.Hamidul

--- app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
+++ app/src/main/java/com/hamidulsarder/visibility/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     CardView bBS, bES, bHS, bTS;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
